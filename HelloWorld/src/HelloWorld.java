e257a040f9957f2731fc41618fe14c4f850fc743
sakshi <sakshi_gupta>
2015-12-29 13:06:08 +0530

    Checking

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello to Git World!!");
 		System.out.println("How are you");
-		System.out.println("How are you Git");
 	}
 }
